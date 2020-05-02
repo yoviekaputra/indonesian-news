45fb0a781dd6c520c2ebafc16cabbc065c084730
yovi.putra <yovi.putra@multipolar.com>
2020-05-02 16:16:07 +0700

    Initial Commit

---
+++ Indonesian News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indonesian News
+//
+//  Created by yoviekaputra on 02/05/20.
+//  Copyright © 2020 multipolar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
